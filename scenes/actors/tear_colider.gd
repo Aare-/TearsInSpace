e1f4d3dcbbd2c8b31b1f3109c882116f5a24efe9
fblost <fblost@gmail.com>
2017-12-09 11:33:50 +0100

    shaky camera

--- scenes/actors/tear_colider.gd
+++ scenes/actors/tear_colider.gd
@@ -1,40 +1,87 @@
 extends RigidBody2D
 
+# Config
+var velocity_range    = Vector2(60, 80)
+var angular_veloc_range = Vector2(-0.5, 0.5)
+
 var blob = null
 
-var velocity        = Vector2()
-var speed_factor    = 1.0
-var scale_min       = 0.4
-var scale_max       = 0.7
+var init_pos = Vector2(0, 0)
+var pos_rad = 0
+var pos_ang = 0
+
+var velocity        = 0
+var angular_velocity = 5.0
+var mov_angle 		= 0.0
+var scale_min       = 0.6
+var scale_max       = 0.6
 var random_scale    = 1
 
 var dying = false
+var animate_death = false
 var fade = 1
+
+onready var spaceship = get_tree().get_root().get_node("root/Spaceship")
+onready var camera    = get_tree().get_root().get_node("root/Spaceship/Camera2D")
 
 func _ready():
 	randomize()
-	velocity         = Vector2( rand_range( -speed_factor, speed_factor )*rand_range( -speed_factor, speed_factor ), rand_range( -speed_factor, speed_factor ) * rand_range( -speed_factor, speed_factor ) )
+	velocity  = rand_range( velocity_range.x, velocity_range.y )	
+	angular_velocity = rand_range(angular_veloc_range.x, angular_veloc_range.y)	
+	
 	set_fixed_process( true )
+	get_tree().get_root().get_node("root/Spaceship/pipes").connect("body_enter",self,"particle_sucked_out")
+	get_tree().get_root().get_node("root/Spaceship/electronics").connect("body_enter",self,"particle_collided_with_wall")
 	
 func _fixed_process( delta ):
+	
+	if init_pos == Vector2(0, 0):
+		init_pos = get_pos()
+
+	pos_rad += velocity * delta
+	pos_ang += angular_velocity * delta
+	
+	set_pos(init_pos + Vector2(cos(pos_ang) * pos_rad, sin(pos_ang) * pos_rad))	
+	
+	blob.set_pos(get_pos())
 
 	if dying:
-		animate_death(delta)
-	else:
-		set_pos(get_pos() + velocity)
-		blob.set_pos(get_pos())
-		check_out_of_screen()
-		var bodies = get_colliding_bodies()
-		if bodies.size():
-			get_node("Particles2D").set_emitting(true)
-			var vector_to_center = get_node("../../Spaceship").get_pos() - get_pos()
-			var angle_to_center  = atan2(vector_to_center.x,vector_to_center.y)
-			get_node("Particles2D").set_param(0,(angle_to_center/(2*PI))*360)
-
-			dying = true
+		if animate_death:
+			animate_death(delta)
+		else:
+			velocity = max(100, velocity - delta * 700)	
+	
+	check_out_of_screen()
+			
+			
+func particle_sucked_out(body):
+	if body != self: return
+	if dying: return
+	dying = true
+	
+	velocity = 700
+	angular_velocity = 0
+	init_pos = get_pos()
+	pos_ang = (init_pos - spaceship.get_pos()).normalized().angle_to(Vector2(1, 0))
+	
+func particle_collided_with_wall(body):
+	if body != self: return
+	if dying: return
+	dying = true
+	animate_death = true
+	
+	velocity = 0
+	angular_velocity = 0
+	
+	get_node("Particles2D").set_emitting(true)
+	var vector_to_center = get_node("../../Spaceship").get_pos() - get_pos()
+	var angle_to_center  = atan2(vector_to_center.x,vector_to_center.y)
+	get_node("Particles2D").set_param(0,(angle_to_center/(2*PI))*360)
+	
+	camera.shake(1.0, 18, 8)
 
 func animate_death( delta ):
-	fade -= delta
+	fade -= delta * 4.0
 	if fade > 0:
 		blob.set_modulate( Color(1,1,1,fade) )
 	else:
@@ -50,14 +97,5 @@ func kill_me():
 	queue_free()
 
 func check_out_of_screen():
-	if (get_pos() - get_node("../../Spaceship").get_pos()).length() >500:
+	if (get_pos() - get_node("../../Spaceship").get_pos()).length() > 700:
 		kill_me()
-#	if   position.x < -100:
-#		 position.x = get_viewport().get_rect().size.x + 100
-#	elif position.x > get_viewport().get_rect().size.x + 100:
-#		 position.x = -100
-#
-#	if   position.y < -100:
-#		 position.y = get_viewport().get_rect().size.y + 100
-#	elif position.y > get_viewport().get_rect().size.y + 100:
-#		 position.y = -100
--- scenes/spaceship.gd
+++ scenes/spaceship.gd
@@ -2,17 +2,18 @@
 extends KinematicBody2D
 
 # Config
-var max_displacement         = Vector2(55, 55)
-var engine_movement_velocity = 4.0
-var engine_launch_threshold  = 10
-var tear_timeout             = 0.5
+var max_displacement         = Vector2(100, 100)
+var engine_movement_velocity = 0.0
+var engine_launch_threshold  = 0
+var tear_timeout             = 2.0
 var max_zoom_out             = 1.5
-var mobility                 = 0.04
+var mobility                 = 0.08
 
 # Variables
 onready var ship_bakcground = get_tree().get_root().get_node("root/Spaceship_Background")
 onready var viewport        = get_tree().get_root().get_node("root/Viewport/Tears_Container")
 onready var camera          = get_tree().get_root().get_node("root/Spaceship/Camera2D")
+onready var astronaut       = get_tree().get_root().get_node("root/Spaceship/Spaceship_Background/Astronaut")
 var tear                    = preload("res://scenes/actors/tear.tscn")
 var tear_collider           = preload("res://scenes/actors/tear_colider.tscn")
 
@@ -21,11 +22,13 @@ var ship_pos_diff  = Vector2(0, 0)
 var curr_pos_diff  = Vector2(0, 0)
 var tears_movement = Vector2(0, 0)
 var tear_timer = 0
+var tear_side_right = true
+var ship_rot = 0
 
-var engine_vector1 = Vector2(1,1).normalized()
-var engine_vector2 = Vector2(-1,1).normalized()
-var engine_vector3 = Vector2(-1,-1).normalized()
-var engine_vector4 = Vector2(1,-1).normalized()
+var engine_vector1 = Vector2(0,1).normalized()
+var engine_vector2 = Vector2(-1,0).normalized()
+var engine_vector3 = Vector2(0,-1).normalized()
+var engine_vector4 = Vector2(1,0).normalized()
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -36,42 +39,38 @@ func _ready():
 func _fixed_process(delta):
 	handle_ship_movement(delta)
 	gen_tear(delta)
+	
+	
 
 func handle_ship_movement(delta):
 	var pos_change = (ship_pos_diff - curr_pos_diff) * mobility
 
 	curr_pos_diff += pos_change
 	set_pos(center_pos + curr_pos_diff)
-
-	# launching the engine
-	var len_diff = abs(curr_pos_diff.length() - max_displacement.length())
-	var engine_power = 0.0
-	if(len_diff < engine_launch_threshold):
-		engine_power = 1.0 - len_diff / engine_launch_threshold;
-		tears_movement -= curr_pos_diff.normalized() * engine_movement_velocity * engine_power
-	emit_engine_exhaust( pos_change )
 	
-	var zoom = camera.get_zoom()
-	zoom[0] = (max_zoom_out - 1.0) * engine_power + 1.0
-	zoom[1] = zoom[0]
-	camera.set_zoom(zoom)
-	get_node("../background").set_scale(zoom)
+	ship_rot += delta * 0.1
+	set_rot(sin(3.14 * ship_rot) * 3.14 * 0.1)
+	astronaut.set_rot(-get_rot())
 
-#	viewport.set_pos(curr_pos_diff * -1 + tears_movement)
+	emit_engine_exhaust( curr_pos_diff )
+	
 
 func emit_engine_exhaust(move_vector):
 	var normalized_pos_change = move_vector.normalized()
-	get_node("Spaceship_Background/top_left").set_param(2 ,100 * clamp(engine_vector1.dot(normalized_pos_change),0,1))
-	get_node("Spaceship_Background/top_right" ).set_param(2 ,100 * clamp(engine_vector2.dot(normalized_pos_change),0,1))
-	get_node("Spaceship_Background/bot_right" ).set_param(2 ,100 * clamp(engine_vector3.dot(normalized_pos_change),0,1))
-	get_node("Spaceship_Background/bot_left").set_param(2, 100 * clamp(engine_vector4.dot(normalized_pos_change),0,1))
+	get_node("Spaceship_Background/top").set_param(2 ,100 * clamp(engine_vector1.rotated(get_rot()).dot(normalized_pos_change),0,1))
+	get_node("Spaceship_Background/right" ).set_param(2 ,100 * clamp(engine_vector2.rotated(get_rot()).dot(normalized_pos_change),0,1))
+	get_node("Spaceship_Background/bottom" ).set_param(2 ,100 * clamp(engine_vector3.rotated(get_rot()).dot(normalized_pos_change),0,1))
+	get_node("Spaceship_Background/left").set_param(2, 100 * clamp(engine_vector4.rotated(get_rot()).dot(normalized_pos_change),0,1))
 
 func gen_tear(delta):
 	tear_timer += delta
-	if tear_timer >= tear_timeout:
-		tear_timer = 0
-		new_tear(get_node("tear_emiter1").get_pos()+get_pos())
-		new_tear(get_node("tear_emiter2").get_pos()+get_pos())
+	while tear_timer >= tear_timeout:
+		tear_timer -= tear_timeout * rand_range(0.8, 1.0)
+		if randi()%2+1 == 1:
+			new_tear(get_node("tear_emiter1").get_pos()+get_pos(), rand_range(0, 3.14) + 3.14 * 0.5)
+		else:
+			new_tear(get_node("tear_emiter2").get_pos()+get_pos(), rand_range(0, 3.14) - 3.14 * 0.5)
+		tear_side_right = !tear_side_right
 
 func _input(event):
 	if (event.type == InputEvent.MOUSE_MOTION):
@@ -80,15 +79,16 @@ func _input(event):
 		ship_pos_diff = ship_pos_diff + mouse_delta
 		ship_pos_diff = ship_pos_diff.clamped(max_displacement.length())
 
-func new_tear(tear_pos):
+func new_tear(tear_pos, tear_ang):
 	var new_tear = tear.instance()
 	get_tree().get_root().get_node("root/Viewport/Tears_Container").add_child( new_tear )
-	new_tear.set_pos( tear_pos )
+	new_tear.set_pos( tear_pos )		
 	
 	var new_tear_collider = tear_collider.instance()
 	get_tree().get_root().get_node("root/colliders").add_child( new_tear_collider )
 	new_tear_collider.blob = new_tear
 	new_tear_collider.set_scale()
 	new_tear_collider.set_pos(tear_pos)
+	new_tear_collider.pos_ang = tear_ang
 
 	
